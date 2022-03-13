71c7d2703204d935ed90151ba5f0d6c3196b1c28
unknown <you@example.com>
2022-03-13 15:33:57 +0530

    Added: Calculating the distance of line

--- src/lineComparisionProblem/LineComparision.java
+++ src/lineComparisionProblem/LineComparision.java
@@ -1,7 +1,15 @@
 package lineComparisionProblem;
 
 public class LineComparision {
+    double Distance(int x1, int y1, int x2, int y2) {
+        double distance;
+        distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        return distance;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Line Comparision Problem");
+        LineComparision linecomparision = new LineComparision();
+        double value = linecomparision.Distance(2, 3, 4, 5);
+        System.out.println("The distance between two points is : " +value);
     }
 }
